92dd48b84c85910b962750d0c935d06871409a05
seunghyonpark <creath.park@gmail.com>
2024-07-24 16:44:51 +0900

    fdasfads

--- src/app/api/order/acceptSellOrder/route.ts
+++ src/app/api/order/acceptSellOrder/route.ts
@@ -52,7 +52,7 @@ export async function POST(request: NextRequest) {
     let message = null;
 
 
-    const msgBody = `[UNOVE] Your order has been accepted! Trade ID: ${tradeId}`;
+    const msgBody = `[UNOVE] Your sell order has been accepted by ${buyer?.nickname}! TID: ${tradeId}`;
 
     message = await client.messages.create({
       ///body: "This is the ship that made the Kessel Run in fourteen parsecs?",
