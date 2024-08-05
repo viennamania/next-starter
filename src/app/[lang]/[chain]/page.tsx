9688888174ee2201f2c796bab3836a6adf689ab8
seunghyonpark <creath.park@gmail.com>
2024-08-05 12:30:05 +0900

    hello world

--- src/app/[lang]/[chain]/page.tsx
+++ src/app/[lang]/[chain]/page.tsx
@@ -970,7 +970,7 @@ export default function Index({ params }: any) {
                     }}
                     className="w-40 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
                   >
-                    {My_Sell_Trades}
+                    {Sell_USDT}
                   </button>
 
                 </div>
