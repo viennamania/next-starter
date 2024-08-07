13fbfb800b6b337da3273fbf430b19b03b7a19d7
seunghyonpark <creath.park@gmail.com>
2024-08-07 16:46:10 +0900

    asdfasdfadsfa

--- src/app/[lang]/[chain]/sell-usdt/page.tsx
+++ src/app/[lang]/[chain]/sell-usdt/page.tsx
@@ -2027,7 +2027,7 @@ export default function Index({ params }: any) {
                                       ////router.push(`/sell-usdt/${item._id}`);
 
                                       // copy to clipboard
-                                      navigator.clipboard.writeText(`https://next.unove.space/${params.lang}/sell-usdt/${item._id}`);
+                                      navigator.clipboard.writeText(`https://next.unove.space/${params.lang}/${params.chain}/sell-usdt/${item._id}`);
                                       toast.success('Link has been copied to clipboard');
 
                                     }}
